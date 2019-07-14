3298270606de81057c770b38d885468b69947c7f
Alenilson Souza <alenilsonsouza@gmail.com>
2019-07-14 14:18:16 -0300

    Atualizando o config

--- config.php
+++ config.php
@@ -18,12 +18,9 @@ if(ENVIRONMENT == 'development') {
 	$config['dbuser'] = 'aleevolu_site';
 	$config['dbpass'] = '#amcqFSg~ZP~';
 }
-define("TITULO_SITE", "Teste");
-define("DESCRICAO", "");
-define("PALAVRAS_CHAVES","");
 define("AUTOR", "Agência Órbita, Ale Evoluções");
 $config['defaut_lang'] = 'pt-br';
-define("TITULO_PAINEL", "Painel CDC - ADM");
+
 
 $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass'],array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
